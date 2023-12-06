0e7fe27fc69d8644f566b78df5667b568c2923ba
Ujjwal Suman <ujjwalsuman007@gmail.com>
2023-12-06 18:18:46 +0530

    Post  request
    CreateTodo

---
+++ config/database.js
@@ -0,0 +1,22 @@
+//its work is to established connection b/w application and db
+
+
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const dbConnect = () => {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB ka Connection is Successful"))
+    .catch( (error) => {
+        console.log("Issue in DB Connection");
+        console.error(error.message);
+        //iska matlab kya h ?
+        process.exit(1);
+    } );
+}
+
+module.exports = dbConnect;
